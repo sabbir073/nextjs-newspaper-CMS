d0f25bf58f84d762954bc20d4fd33c165944439c
sabbir073 <md.sabbir073@gmail.com>
2024-11-20 15:23:17 +0600

    image url updated

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
           },
           {
             protocol: 'https',
-            hostname: "images.unsplash.com" ,
+            hostname: "dainikdhaka24.com" ,
           },
         ],
       },
