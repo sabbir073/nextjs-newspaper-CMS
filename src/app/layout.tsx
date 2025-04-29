a800af5017963a086494e1837a08241c3e4ead26
sabbir073 <md.sabbir073@gmail.com>
2025-04-30 02:13:46 +0600

    added speed insigh

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { SessionProvider } from "next-auth/react";
 import "./globals.css";
 import localFont from "next/font/local";
 import { ReactNode } from "react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const solaiman = localFont({
   src: [{ path: "./fonts/SolaimanLipi.ttf", weight: "400" }],
@@ -16,7 +17,9 @@ interface RootLayoutProps {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
+    
     <SessionProvider>
+      <SpeedInsights />
       <html lang="en" className={`${solaiman.variable} font-solaiman `}>
         <body>{children}</body>
       </html>
